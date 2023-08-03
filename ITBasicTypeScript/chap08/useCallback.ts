81ca3cd9119429c212ee3a26de3dfc772eb38a5d
hashimoto-motchan <hashimoto20540@gmail.com>
2023-08-03 10:02:02 +0900

    study

---
+++ ITBasicTypeScript/chap08/useCallback.ts
@@ -0,0 +1,10 @@
+export{}
+
+function showRoundedElement(currentValue: number, index: number, array: number[]) {
+    const roundElement = Math.round(currentValue);
+    console.log(`${index + 1}個目の要素${currentValue}の丸め処理後:
+    ${roundElement}`);
+}
+
+const numList = [45.112, 78.567, 66.891, 12.223, 28.341];
+numList.forEach(showRoundedElement);
---
+++ ITBasicTypeScript/chap08/useOverloads.ts
@@ -0,0 +1,17 @@
+export{}
+
+function descOneself(name: string, massage: string);
+function descOneself(name: string, month: number, day: number);
+function descOneself(name: string, msgOrMonth: number|string, day?: number) {
+    let desc = `こんにちは、${name}です。`;
+    if(typeof msgOrMonth == "string") {
+        desc += msgOrMonth;
+    } else {
+        desc += `${msgOrMonth} 月${day}日生まれです。`
+    }
+    console.log(desc);
+}
+
+descOneself("江藤", "よろしくお願いします！");
+descOneself("江藤",6, 12);
+// descOneself("江藤",6);
